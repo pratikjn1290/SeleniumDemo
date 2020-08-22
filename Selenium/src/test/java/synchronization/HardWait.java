00d0eb841d0eaa17dfd88582d054b8688b8efbdd
pratikjn1290 <pratik.jain2@outlook.com>
2020-08-22 21:42:22 +0530

    HardWait

---
+++ Selenium/src/test/java/synchronization/HardWait.java
@@ -0,0 +1,35 @@
+package synchronization;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class HardWait {
+
+
+	@Test
+	public void hard_wait()
+
+	{	
+		System.setProperty("webdriver.gecko.driver", "C:/Users/Pratik/Desktop/RestAssured/Selenium/Drivers/geckodriver.exe");	
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://corpository.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+
+		WebElement Signin = driver.findElement(By.xpath("//a[text() = 'Sign In']"));
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Signin.click();
+
+	}
+}
+
